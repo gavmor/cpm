61335160ad12915185f4b532ef05f449939de174
Gavin Morgan <morgan.gavin@gmail.com>
2024-10-02 17:11:12 -0700

    await memo

--- src/constructInstallationPlan.ts
+++ src/constructInstallationPlan.ts
@@ -1,5 +1,14 @@
 import { test, expect, equals } from "@benchristel/taste";
-import { Herd, InstallationPlan, spreadInstallation } from "./install";
+import { isArray } from "util";
+
+
+export type InstallationPlan = DependencyInstallation[];
+type Version = `${number}.${number}.${number}`;
+interface Dependency { name: string; version: Version; }
+export type Herd = Record<string, Version>;
+export interface DependencyInstallation extends Dependency {
+    parentDirectory?: string;
+}
 
 export async function constructInstallationPlan(
     topLevelDependencies: Herd): Promise<InstallationPlan> {
@@ -12,15 +21,39 @@ export async function constructInstallationPlan(
     // }, currentPlan);
 }
 
+export function planetize(topLevelDependencies: Herd): InstallationPlan {
+    return Object
+        .entries(topLevelDependencies)
+        .map(([name, version]) => ({
+            name,
+            version,
+            parentDirectory: "node_modules"
+        }));
+}
+
+export const spreadInstallation = async (memo: InstallationPlan, i: DependencyInstallation): Promise<InstallationPlan> => { 
+    const response = await fetch(registryPath("@benchristel/taste", "0.6.0"));
+    const { dependencies }: { dependencies: Herd } = await response.json()
+
+    return [
+        ...await memo,
+        ...planetize(dependencies)
+    ]
+};
+
+export function registryPath(name: string, version: string): string | URL | Request {
+    return `https://registry.npmjs.org/${name}@${version}`;
+}
+
 test("constructInstallationPlan", {
     async "by default, assigns dependencies to node_modules"() {
-        stubFetch({ dependencies: {} as Herd });
+        stubFetch({ dependencies: { } });
 
         expect(await constructInstallationPlan({
-            "foo": "1.2.3", "bar": "1.0.1",
+            "strawberry": "1.2.3", "mango": "1.0.1",
         }), equals, [
-            { name: "foo", version: "1.2.3", parentDirectory: "node_modules" },
-            { name: "bar", version: "1.0.1", parentDirectory: "node_modules" }
+            { name: "strawberry", version: "1.2.3", parentDirectory: "node_modules" },
+            { name: "mango", version: "1.0.1", parentDirectory: "node_modules" }
         ]);
     },
     async "given a depency with dependencies, adds their deps to the plan"() {
@@ -35,23 +68,21 @@ test("constructInstallationPlan", {
     },
 });
 
-export function planetize(topLevelDependencies: Herd): InstallationPlan {
-    return Object
-        .entries(topLevelDependencies)
-        .map(([name, version]) => ({
-            name,
-            version,
+test("descendPlan", {
+    async "fetches the dependencies of a dependency installation, and merges them into a plan"() {
+        stubFetch()
+
+        expect(await spreadInstallation([], { name: "qux", version: "0.4.2" }), equals, [{
+            name: "child",
+            version: "1.0.1",
             parentDirectory: "node_modules"
-        }));
-}
+        }])
+    }
+})
 
 export function stubFetch(json?) {
     // @ts-expect-error
     global.fetch = (input, init) => new Promise(resolve => resolve(new Response(JSON.stringify(json || {
         dependencies: { "child": "1.0.1" }
     }))));
-}
-
-export function registryPath(name: string, version: string): string | URL | Request {
-    return `https://registry.npmjs.org/${name}@${version}`;
 }
--- src/install.ts
+++ src/install.ts
@@ -1,40 +1,12 @@
 import { readFileSync } from "fs";
-import { test, expect, equals } from "@benchristel/taste"
-import { constructInstallationPlan, registryPath, planetize, stubFetch } from "./constructInstallationPlan";
+import { constructInstallationPlan, Herd } from "./constructInstallationPlan";
 
 export async function install({ packageFile }) {
     constructInstallationPlan(readDeps(packageFile))
 }
 
-export type InstallationPlan = DependencyInstallation[]
-type Version = `${number}.${number}.${number}`;
-interface Dependency { name: string, version: Version }
-interface DependencyInstallation extends Dependency {
-    parentDirectory?: string
-}
-
-function readDeps(packageFilePath: string): Herd {
+function readDeps(packageFilePath: string): Hegird {
     return JSON.parse(readFileSync(packageFilePath, { encoding: 'utf-8' }))
         .dependencies;
 }
-
-export type Herd = Record<string, Version>;
-export const spreadInstallation = async (memo: InstallationPlan, i: DependencyInstallation): Promise<InstallationPlan> => {
-    const response = await fetch(registryPath("@benchristel/taste", "0.6.0"));
-    const { dependencies }: { dependencies: Herd } = await response.json()
-    return [...memo, ...planetize(dependencies)]
-};
-
-test("descendPlan", {
-    async "fetches the dependencies of a dependency installation, and merges them into a plan"() {
-        stubFetch()
-
-        expect(await spreadInstallation([], { name: "qux", version: "0.4.2" }), equals, [{
-            name: "child",
-            version: "1.0.1",
-            parentDirectory: "node_modules"
-        }])
-    }
-})
-
 
