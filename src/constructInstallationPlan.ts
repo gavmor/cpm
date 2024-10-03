3687063b44e649e6b345123c1f5eac34f6f57b0a
Gavin Morgan <morgan.gavin@gmail.com>
2024-10-02 17:42:12 -0700

    recur

--- src/constructInstallationPlan.ts
+++ src/constructInstallationPlan.ts
@@ -1,4 +1,5 @@
 import { test, expect, equals } from "@benchristel/taste";
+import { match, stub } from "sinon";
 import { isArray } from "util";
 
 
@@ -14,12 +15,18 @@ export async function constructInstallationPlan(
     topLevelDependencies: Herd): Promise<InstallationPlan> {
     const currentPlan = planetize(topLevelDependencies);
     return currentPlan.reduce<InstallationPlan>(spreadInstallation, currentPlan);
-
-    // return await currentPlan.reduce(async (plan, {name, version}) => {
-    //     JSON.stringify(await fetch(registryPath(name, version)))
-    //     return plan
-    // }, currentPlan);
 }
+
+export const spreadInstallation = async (memo: InstallationPlan, { name, version }: DependencyInstallation): Promise<InstallationPlan> => {
+    const response = await fetch(registryPath(name, version));
+    if (!response) { return memo; }
+
+    const { dependencies }: { dependencies: Herd } = await response.json()
+    return [
+        ...await memo,
+        ...await constructInstallationPlan(dependencies)
+    ]
+};
 
 export function planetize(topLevelDependencies: Herd): InstallationPlan {
     return Object
@@ -31,54 +38,65 @@ export function planetize(topLevelDependencies: Herd): InstallationPlan {
         }));
 }
 
-export const spreadInstallation = async (memo: InstallationPlan, i: DependencyInstallation): Promise<InstallationPlan> => { 
-    const response = await fetch(registryPath("@benchristel/taste", "0.6.0"));
-    const { dependencies }: { dependencies: Herd } = await response.json()
-
-    return [
-        ...await memo,
-        ...planetize(dependencies)
-    ]
-};
-
 export function registryPath(name: string, version: string): string | URL | Request {
     return `https://registry.npmjs.org/${name}@${version}`;
 }
 
 test("constructInstallationPlan", {
-    async "by default, assigns dependencies to node_modules"() {
-        stubFetch({ dependencies: { } });
+    // async "by default, assigns dependencies to node_modules"() {
+    //     stubFetch({ dependencies: {} });
+
+    //     expect(await constructInstallationPlan({
+    //         "strawberry": "1.2.3", "mango": "1.0.1",
+    //     }), equals, [
+    //         { name: "strawberry", version: "1.2.3", parentDirectory: "node_modules" },
+    //         { name: "mango", version: "1.0.1", parentDirectory: "node_modules" }
+    //     ]);
+    // },
+    // async "given a depency with dependencies, adds their deps to the plan"() {
+    //     stubFetch();
+
+    //     expect(await constructInstallationPlan({
+    //         "parent": "1.2.3",
+    //     }), equals, [
+    //         { name: "parent", version: "1.2.3", parentDirectory: "node_modules" },
+    //         { name: "child", version: "1.0.1", parentDirectory: "node_modules" }
+    //     ]);
+    // },
+    async "given a dependency tree at least two levels deep, adds their deps to the plan"() {
+        const parentMatcher = match((url) => url.includes('parent'));
+        const childMatcher = match((url) => url.includes('child'));
+
+        const fetchStub = stub<any, any>(global, 'fetch');
+        fetchStub.withArgs(parentMatcher).resolves({
+            json: () => Promise.resolve({ dependencies: { "child": "1.0.1" } })
+        });
+
+        fetchStub.withArgs(childMatcher).resolves({
+            json: () => Promise.resolve({ dependencies: { "grandkid": "4.0.4" } })
+        });
 
         expect(await constructInstallationPlan({
-            "strawberry": "1.2.3", "mango": "1.0.1",
-        }), equals, [
-            { name: "strawberry", version: "1.2.3", parentDirectory: "node_modules" },
-            { name: "mango", version: "1.0.1", parentDirectory: "node_modules" }
-        ]);
-    },
-    async "given a depency with dependencies, adds their deps to the plan"() {
-        stubFetch();
-
-        expect(await constructInstallationPlan({
-            "parent": "1.2.3",
+            "parent": "1.2.3"
         }), equals, [
             { name: "parent", version: "1.2.3", parentDirectory: "node_modules" },
-            { name: "child", version: "1.0.1", parentDirectory: "node_modules" }
+            { name: "child", version: "1.0.1", parentDirectory: "node_modules" },
+            { name: "grandkid", version: "4.0.4", parentDirectory: "node_modules" },
         ]);
     },
 });
 
-test("descendPlan", {
-    async "fetches the dependencies of a dependency installation, and merges them into a plan"() {
-        stubFetch()
+// test("spreadInstallation", {
+//     async "fetches the dependencies of a dependency installation, and merges them into a plan"() {
+//         stubFetch()
 
-        expect(await spreadInstallation([], { name: "qux", version: "0.4.2" }), equals, [{
-            name: "child",
-            version: "1.0.1",
-            parentDirectory: "node_modules"
-        }])
-    }
-})
+//         expect(await spreadInstallation([], { name: "qux", version: "0.4.2" }), equals, [{
+//             name: "child",
+//             version: "1.0.1",
+//             parentDirectory: "node_modules"
+//         }])
+//     }
+// })
 
 export function stubFetch(json?) {
     // @ts-expect-error
